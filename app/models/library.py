90ee655eecce261bc2a7fe5094becbfb90b59382
Marcel Fonseca <marcel.resende@yahoo.com.br>
2022-04-07 23:24:04 -0300

    Update library.py

--- app/models/library.py
+++ app/models/library.py
@@ -9,21 +9,21 @@ class Library(db.Model):
     pay_id = db.Column(db.String(255), unique=True)
     status = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    material_id = db.Column(db.Integer, db.ForeignKey('material.id'), nullable=False)
+    payment_link = db.Column(db.Integer, db.ForeignKey('material.id'), nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.datetime.now())
     material = db.relationship('Material', back_populates='library')
 
-    def __init__(self, pay_id, status, user_id, material_id):
+    def __init__(self, pay_id, status, user_id, payment_link):
         self.pay_id = pay_id
         self.status = status
         self.user_id = user_id
-        self.material_id = material_id
+        self. payment_link = payment_link
 
 
 class LibrarySchema(ma.Schema):
 
     class Meta:
-        fields = ('id', 'pay_id', 'status', 'user_id', 'material_id', 'created_on')
+        fields = ('id', 'pay_id', 'status', 'user_id', 'payment_link', 'created_on')
 
 
 library_schema = LibrarySchema()
